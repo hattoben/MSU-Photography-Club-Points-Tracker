91d305ae15cb503d386bf7abea33eaf455f8ebec
benhatto <benjamin.hatto@gmail.com>
2025-09-06 11:13:17 -0400

    updated 9.6.25

--- final points calculation.py
+++
@@ -1,8 +0,0 @@
-from involve import concatenateInvolveData
-from events import concatenateEventsData
-import pandas as pd
-
-involve_data = concatenateInvolveData("involve data")
-events_data = concatenateEventsData("events data")
-
-grand_total = pd.DataFrame()
